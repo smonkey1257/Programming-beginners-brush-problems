8c138e68665bfc974425a9a8ebc7820e97af7b71
smonkey1257 <15219574073@139.com>
2023-06-10 13:35:31 +0800

    C语言初学者题目值简单输出

---
+++ Simple output/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/* 第一题：BC1 Hello Nowcoder */
+#include <stdio.h>
+
+int main() {
+    printf("Hello Nowcoder!\n");
+    return 0;
+}
+
+/* 第二题 ：BC2 小飞机 */
+#include <stdio.h>
+
+int main() {
+    printf("     **\n");
+    printf("     **\n");
+    printf("************\n");
+    printf("************\n");
+    printf("    *  *    \n");
+    printf("    *  *    \n");
+    return 0;
+}
