7525a60108f98b884b213dc6770ce5a7460e9940
smonkey1257 <15219574073@139.com>
2023-06-11 11:30:34 +0800

    基础数据类型之整型/浮点型/字符型

---
+++ Basic_type_1/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/* 第一道题：BC3 牛牛学说话之-整数 */
+#include <stdio.h>
+
+int main() {
+    //32位有符号整数：int
+    int num = 0;
+    scanf("%d", &num);
+    printf("%d", num);
+    return 0;
+}
+
+
+/* 第一道题：BC4 牛牛学说话之-浮点数 */
+#include <stdio.h>
+
+int main() {
+    float num = 0.0f;
+    scanf("%f", &num);
+    printf("%.3f", num);
+    return 0;
+}
+
+
+/* 第一道题：BC5 牛牛学说话之-字符 */
+#include <stdio.h>
+
+int main() {
+    char num = 0;
+    scanf("%c", &num);
+    printf("%c", num);
+    return 0;
+}
