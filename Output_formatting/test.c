d2d64ba5f5c574972f97f09edb19f3caf0940b10
smonkey1257 <15219574073@139.com>
2023-06-16 21:49:43 +0800

    C语言输出格式化入门编程题

--- Output_formatting/test.c
+++ Output_formatting/test.c
@@ -8,3 +8,30 @@ int main() {
     printf("%15d\n", num);
     return 0;
 }
+
+
+/* BC17 缩短二进制 */
+#include <stdio.h>
+
+int main() {
+    int num = 1234;
+    // printf("%#o ", num);
+    // printf("%#X\n", num);
+    printf("%#o %#X\n", num, num);
+    return 0;
+}
+
+
+/* BC18 牛牛的空格分隔 */
+#include <stdio.h>
+
+int main() {
+    char str = 0;
+    int num1 = 0;
+    float num2 = 0.0f;
+    scanf("%c\n%d\n%f", &str, &num1, &num2);
+    printf("%c %d %f", str, num1, num2);
+    return 0;
+}
+
+
