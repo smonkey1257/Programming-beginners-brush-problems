b5d55721b0fddedd709da5d4ecb240d5dca98efa
smonkey1257 <15219574073@139.com>
2023-06-18 22:14:56 +0800

    C语言基本运算入门编程题

---
+++ Basic_operator/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+/* BC21 牛牛学加法 */
+#include <stdio.h>
+
+int main() {
+    int a = 0, b = 0;
+    scanf("%d%d", &a, &b);
+    printf("%d\n", a + b);
+    return 0;
+}
+
+
+/* BC22 牛牛学除法 */
+#include <stdio.h>
+
+int main() {
+    int a = 0, b = 0;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a / b);
+    return 0;
+}
+
+
+/* */
