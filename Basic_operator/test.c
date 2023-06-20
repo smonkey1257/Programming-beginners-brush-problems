fe0628f7d34da793712005417eb8a3413f370dec
smonkey1257 <15219574073@139.com>
2023-06-20 10:05:03 +0800

    C语言基本运算入门编程题

--- Basic_operator/test.c
+++ Basic_operator/test.c
@@ -43,3 +43,38 @@ int main() {
     return 0;
 }
 
+
+/* BC25 牛牛买电影票 */
+#include <stdio.h>
+
+int main() {
+    int x = 0;
+    scanf("%d", &x);
+    printf("%d00", x);
+    return 0;
+}
+//剑走偏锋，但优点在于可以表示更多的数字
+
+
+/* BC26 计算带余除法 */
+#include <stdio.h>
+
+int main() {
+    int a = 0, b = 0;
+    scanf("%d %d", &a, &b);
+    printf("%d %d", a / b, a % b);
+    return 0;
+}
+
+
+/* BC27 整数的个位 */
+#include <stdio.h>
+
+int main() {
+    int num = 0;
+    scanf("%d", &num);
+    printf("%d", num % 10);
+    return 0;
+}
+
+
